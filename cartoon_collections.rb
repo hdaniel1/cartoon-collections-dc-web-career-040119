5ffb52114bd71d74989a4e2516f2b7f4c47e49ae
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 20:13:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ def find_the_cheese(string)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   string.find do |word|
-    cheese_types.include(word)
+    cheese_types.include?(word)
   end
 end
